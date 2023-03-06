95db7dd4666c759fd1b1ae69ae6662d3a0c84dc4
Juciano <juciano.junior@lais.huol.ufrn.br>
2023-03-06 19:55:10 +0000

    :package: build: packages and configurations

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
-const App = () => {
+import { BrowserRouter } from "react-router-dom";
 
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
+
+const App = () => {
   return (
-    <div>
-      3D Portifolio
-    </div>
-  )
+    <BrowserRouter>
+      <div className='relative z-0 bg-primary'>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className='relative z-0'>
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
