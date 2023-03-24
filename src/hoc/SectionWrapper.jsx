15cb167e56a790d34b246ecdd74bf1ccb3241ff4
Juciano <juciano.junior@lais.huol.ufrn.br>
2023-03-24 13:53:24 +0000

    :wrench: fix: works not showing

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -1,13 +1,32 @@
 import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
 import { styles } from "../styles";
 import { staggerContainer } from "../utils/motion";
 
 const SectionWrapper = (Component, idName, work) =>
   function HOC() {
+    const [mobile, setMobile] = useState(false);
+
+    useEffect(() => {
+      const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+      setMobile(mediaQuery.matches);
+      console.log(mediaQuery.matches);
+
+      const handleMediaQueryChange = (event) => {
+        setMobile(event.matches);
+      };
+
+      mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+      return () => {
+        mediaQuery.removeEventListener("change", handleMediaQueryChange);
+      };
+    }, []);
     return (
       <motion.section
         variants={staggerContainer()}
-        initial={work ? "visible" : "hidden"}
+        initial={work == true ? "visible" : "hidden"}
         whileInView="show"
         viewport={{ once: true, amount: 0.25 }}
         className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
