b1723d41ae2e35fe581f8175985623deba857b0a
Juciano <juciano.junior@lais.huol.ufrn.br>
2023-03-24 13:38:16 +0000

    :wrench: fix: Computer and works were not desplaying

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -2,12 +2,12 @@ import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { staggerContainer } from "../utils/motion";
 
-const SectionWrapper = (Component, idName) =>
+const SectionWrapper = (Component, idName, work) =>
   function HOC() {
     return (
       <motion.section
         variants={staggerContainer()}
-        initial="hidden"
+        initial={work ? "visible" : "hidden"}
         whileInView="show"
         viewport={{ once: true, amount: 0.25 }}
         className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
